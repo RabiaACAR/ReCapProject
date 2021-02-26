6c3e6b9d421e2cc708d24cfbf7bafdaf2537385e
Rabia Acar <rabiaaaacar@hotmail.com>
2021-02-26 17:12:21 +0300

    10.Ders Sonu

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            if (rental.ReturnDate==null)
+            {
+                return new ErrorResult(Messages.RentalInvalid);
+            }
+            return new SuccessResult(Messages.RentalAdded);
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult(Messages.RentalDeleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            _rentalDal.GetAll();
+            return new SuccessDataResult<List<Rental>>(Messages.RentalListed); 
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult(Messages.RentalUpdated);
+
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,16 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Araba ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string CarListed = "Arabalar listelendi";
+
+        public static string CustomerAdded = "Müşteri EKlendi.";
+
+        public static string CustomerDeleted { get; internal set; }
+        public static object CustomerListed { get; internal set; }
+        public static string CustomerUpdated { get; internal set; }
+        public static string RentalInvalid { get; internal set; }
+        public static string RentalAdded { get; internal set; }
+        public static string RentalDeleted { get; internal set; }
+        public static string RentalListed { get; internal set; }
+        public static string RentalUpdated { get; internal set; }
     }
 }
